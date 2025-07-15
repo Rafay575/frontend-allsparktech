026377abddad43d6410c81615af26ecf0a053570
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2025-07-15 23:41:25 +0500

    ..

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 const NotFoundPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4">
-      <h1 className="text-9xl font-extrabold text-red-500 mb-4 animate-pulse">404</h1>
+      <h1 className="text-9xl font-extrabold text-[#384BFF] mb-4 animate-pulse">404</h1>
       <p className="text-2xl md:text-3xl font-semibold mb-6 text-gray-800">Oops! Page not found.</p>
       <p className="text-gray-600 mb-8 text-center max-w-md">
         We can’t seem to find the page you’re looking for.
       </p>
-      <Link href="/" className="inline-block px-6 py-3 bg-red-500 text-white rounded-lg hover:bg-red-600 transition">
+      <Link href="/" className="inline-block px-6 py-3 bg-[#384BFF] text-white rounded-lg hover:bg-blue-600 transition">
         Go Home
       </Link>
     </div>
