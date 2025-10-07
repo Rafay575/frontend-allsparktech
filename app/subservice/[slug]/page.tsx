e5f5c2f9dce6d7f983106047da08c41c6235b8b2
Abdullah <abdullahmug2332@gmail.com>
2025-10-07 11:38:00 -0700

    make hero slider

--- app/subservice/[slug]/page.tsx
+++ app/subservice/[slug]/page.tsx
@@ -17,7 +17,7 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0; 
 
 type PageProps = {
-  params: { slug: string };
+   params: Promise<{ slug: string }>;
 };
 
 async function fetchSubservice(slug: string) {
