d69064688153caa6e4455f3112cc94e36229b010
Abdullah <abdullahmug2332@gmail.com>
2025-10-10 12:43:39 -0700

    make adjustments in process and esponsoive ness

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',         // allow port 5000
-        pathname: '/images/**',
-      }
       // {
-      //   protocol: 'https',
-      //   hostname: 'vercel.libererllc.com',
-      //   pathname: '/images/**', // Optionally restrict to specific paths
+      //   protocol: 'http',
+      //   hostname: 'localhost',
+      //   port: '5000',         // allow port 5000
+      //   pathname: '/images/**',
       // }
+      {
+        protocol: 'https',
+        hostname: 'vercel.libererllc.com',
+        pathname: '/images/**', // Optionally restrict to specific paths
+      }
     ],
     
   },
