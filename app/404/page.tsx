31318592a55dc7fdac4c44d75b2c556cf30a3cde
Abdullah <abdullahmug2332@gmail.com>
2025-10-13 10:55:02 -0700

    add change in blogs

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-const page = () => {
+export default function NotFound() {
   return (
-    <div>not found page</div>
-  )
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold mb-2">404 — Page Not Found</h1>
+      <p className="text-gray-600">The page you are looking for doesn’t exist.</p>
+    </div>
+  );
 }
-
-export default page
