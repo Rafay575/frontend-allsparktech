12866815b5cec9dfc2249a420d861b4534d61e84
Abdullah <abdullahmug2332@gmail.com>
2025-10-13 10:47:13 -0700

    add change in blogs

---
+++ app/NotFound/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold mb-2">404 — Page Not Found</h1>
+      <p className="text-gray-600">The page you are looking for doesn’t exist.</p>
+    </div>
+  );
+}
